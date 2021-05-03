485ec7d0ea39cd69edb059191a7f22172b143120
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-04 01:53:56 +0530

    Add files via upload

---
+++ Class_3.java
@@ -0,0 +1,23 @@
+//Program to illustrate Comments in  java
+public class Class_3 {
+    public static void main(String[] args)
+    {
+        
+        // This is  a single line comment in a program
+        /*
+        This is  a multi line comment that we see
+        Remember comments are genrally used by 
+        programmer to understand a particular 
+        problem or explain it so that other
+        programmer can understand just by
+        the code given to them   
+        */
+        /**
+         * This type of Comment is a documentational programming
+         * <h1>Documents and tags can be also written here</h1>
+         * Blah blah#####%%%%^&&*()_
+         * =-09876456562@#$%*&^%$#@
+         */
+
+    }
+}
