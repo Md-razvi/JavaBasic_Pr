8a372b97f71641f0df06ea88f1ba6ecbcf9ee99b
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-04 02:04:07 +0530

    Add files via upload

---
+++ Class_4.java
@@ -0,0 +1,13 @@
+public class Class_4 {
+    public static void main(String[] args){
+        // Defining variable int,bool,char and float 
+        int num1=45;// Stores an integer value
+        boolean b=true;// Stores either true or false values
+        char alphabet='A'; // Stores a single character/letter or Ascii value
+        float c=31.4f;// Stores fractional numbers but requires f at its end
+        System.out.println(num1);
+        System.out.println(b);
+        System.out.println(alphabet);
+        System.out.println(c);
+    }
+}
